f89787c665f03bbe20dee0fe800a41151a522ed3
Benjamin Ledoux <bledoux2002@gmail.com>
2026-04-01 10:40:46 -0500

    webcam display

---
+++ main.py
@@ -0,0 +1,20 @@
+import cv2
+# from ultralytics import YOLO
+
+def main():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
+
+    while True:
+        ret, img = cap.read()
+        cv2.imshow('Webcam', img)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
